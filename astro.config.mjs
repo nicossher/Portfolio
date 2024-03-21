62d7879aeb25acd33905428216dcffb7e77634f5
NicoSher <nsher@frba.utn.edu.ar>
2024-03-21 14:28:40 -0300

    errores de imagenes no visibles en el build arreglado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), robotsTxt()],
+  site: "https://porfolio-sher.dev/",
+  build: {
+    outDir: "public",
+  },
 });
